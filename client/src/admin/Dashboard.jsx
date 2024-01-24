270e427e600e3166c156bd2a97a703f9698204b2
yashksaini <yashdms1@gmail.com>
2024-01-24 07:22:10 +0530

    style: :art: Changed the Active player card

--- client/src/admin/Dashboard.jsx
+++ client/src/admin/Dashboard.jsx
@@ -53,12 +53,12 @@ const Dashboard = ({ socket }) => {
           ?.map((user) => (
             <div
               key={user.userId}
-              className="flex items-center justify-between w-full px-4 py-2 md:py-4 bg-background border text-dark1 rounded mt-4 gap-2"
+              className="flex items-center justify-between w-full px-4 py-2 md:py-4 bg-background border text-dark1 rounded mt-4 gap-2 relative"
             >
-              <span className="text-xl font-bold flex-1">
-                {user.fullName}
-                <i className="fa-regular fa-circle-dot ml-2 text-green-500 text-sm"></i>
+              <span className="absolute top-[-12px] left-[-12px] w-6 h-6 flex justify-center items-center bg-background border border-green-500 rounded-full">
+                <i className="fa-regular fa-circle-dot  text-green-500 text-sm"></i>
               </span>
+              <span className="text-xl font-bold flex-1">{user.fullName}</span>
               {user.userId !== userId ? (
                 <button
                   className="border rounded-md text-xs px-3 py-2 bg-white font-semibold"
@@ -72,7 +72,7 @@ const Dashboard = ({ socket }) => {
         {activeUsers?.filter((data) => data.fullName !== fullName).length ===
         0 ? (
           <div className="text-center text-gray-500 my-4">
-            No active users right now
+            No active players right now
           </div>
         ) : null}
       </div>
